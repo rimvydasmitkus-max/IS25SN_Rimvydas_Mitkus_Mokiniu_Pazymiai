d9134a8ff65459243cffa1fd1304371fee9950de
rimvydasmitkus-max <rimvydasmitkus@gmail.com>
2026-04-06 14:09:40 +0300

    Pridetas_pazymiu_perziurejimas_ir_atnaujinimas.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,9 @@ int main() {
         cout << "\n--- MOKINIU PAZYMIU SISTEMA ---\n";
         cout << "1. Prideti mokini\n";
         cout << "2. Rodyti visus mokinius\n";
-        cout << "3. Baigti darba\n";
+        cout << "3. Rodyti vieno mokinio pazymius\n";
+        cout << "4. Atnaujinti pazymi\n";
+        cout << "5. Baigti darba\n";
         cout << "Pasirinkite: ";
         cin >> pasirinkimas;
 
@@ -64,7 +66,71 @@ int main() {
                 break;
             }
 
-            case 3:
+            case 3: {
+                if (mokiniuKiekis == 0) {
+                    cout << "Mokiniu sarasas tuscias.\n";
+                    break;
+                }
+
+                int nr;
+                cout << "Iveskite mokinio numeri: ";
+                cin >> nr;
+
+                if (nr < 1 || nr > mokiniuKiekis) {
+                    cout << "Tokio mokinio nera.\n";
+                    break;
+                }
+
+                int i = nr - 1;
+                cout << "Mokinys: " << vardai[i] << endl;
+                cout << "Pazymiai: ";
+                for (int j = 0; j < pazymiuKiekis[i]; j++) {
+                    cout << pazymiai[i][j] << " ";
+                }
+                cout << endl;
+                break;
+            }
+
+            case 4: {
+                if (mokiniuKiekis == 0) {
+                    cout << "Mokiniu sarasas tuscias.\n";
+                    break;
+                }
+
+                int nr;
+                cout << "Iveskite mokinio numeri: ";
+                cin >> nr;
+
+                if (nr < 1 || nr > mokiniuKiekis) {
+                    cout << "Tokio mokinio nera.\n";
+                    break;
+                }
+
+                int i = nr - 1;
+
+                cout << "Mokinys: " << vardai[i] << endl;
+                for (int j = 0; j < pazymiuKiekis[i]; j++) {
+                    cout << j + 1 << ". " << pazymiai[i][j] << endl;
+                }
+
+                int pazNr, naujas;
+                cout << "Kuri pazymi norite keisti? ";
+                cin >> pazNr;
+
+                if (pazNr < 1 || pazNr > pazymiuKiekis[i]) {
+                    cout << "Tokio pazymio nera.\n";
+                    break;
+                }
+
+                cout << "Iveskite nauja pazymi: ";
+                cin >> naujas;
+
+                pazymiai[i][pazNr - 1] = naujas;
+                cout << "Pazymys atnaujintas.\n";
+                break;
+            }
+
+            case 5:
                 cout << "Programa baigta.\n";
                 break;
 
@@ -72,7 +138,7 @@ int main() {
                 cout << "Neteisingas pasirinkimas.\n";
         }
 
-    } while (pasirinkimas != 3);
+    } while (pasirinkimas != 5);
 
     return 0;
 }
